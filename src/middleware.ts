2cc927b57c7b8208985c5db8acdff9b2178bb857
Roshan V Razak <roshanvrazak@gmail.com>
2026-04-01 08:36:36 +0100

    feat: add middleware and auth api routes

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith("/api/auth");
+  const isPublicRoute = ["/login", "/signup"].includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) return;
+
+  if (isPublicRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL("/", nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/api/:path*"],
+};
